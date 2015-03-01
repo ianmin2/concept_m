fafae95f320ae00da53a410b4b672723c1a22ea1
Ian Innocent <ianmin2@live.com>
2015-03-01 06:34:53 +0300

    :construction: from :earth_africa:

--- www/js/panel.js
+++ www/js/panel.js
@@ -11,18 +11,39 @@ function panelQRCapture( evt) {
            /* Do the data analysis here!! */
             $path = localStorage.getItem("requestDomain");
             
+            //console.log( "path says: " + $path );
+            
             switch ( $path ){
             
                     case "vehicles":
-                    /* Fetch the data concerning the relevant vehicle   */
+                        /* Fetch the data concerning the relevant vehicle   */
+                        $({
+                            url: __URL__+"proc/police_panel.php",
+                            data: {},
+                            dataType: "jsonp",
+                            success: function ( resp ){
+                                
+                                r = JSON.stringify( resp );
+                                $("#body").html( r );
+                                
+                            }
+                        });
                     break;
                     
                     case "drivers":
-                    /* Fetch the data concerning the relevant driver */
+                        /* Fetch the data concerning the relevant driver */
+                    
                     break;
                     
                     case "notifications":
-                    /* Fetch the relevant officer notifications */
+                        /* Fetch the relevant officer notifications */
+                    
+                    break;
+                    
+                    default:
+                    
+                        /* request boo boo */
+                    
                     break;
                     
             }
